c94f5b9418f074f757142dc3e0468523586a360e
Amit Swain <business.amitswain@gmail.com>
2023-10-25 17:59:26 +0530

    Added G-Mail login UI project

---
+++ 04. Exploring Relative layouts/UI - Projects/GMail_Login_UI/app/src/main/java/com/androidkickofftraining/g_mail_login_ui/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.androidkickofftraining.g_mail_login_ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.gmail_login)
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        println("Application has started!")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        println("Application is running!")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        println("Application has paused and is in background!")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        println("Application has stopped and is still in background!")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        println("Application has re-started again and it is running!")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        println("Application is closed and it is not running in background as well!")
+    }
+}
