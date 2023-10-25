a5e684b3491c976bb650ac5daa2d4d3e99563188
Amit Swain <business.amitswain@gmail.com>
2023-10-26 01:05:29 +0530

    Instagram latest login page UI added and their logos as well

---
+++ 04. Exploring Relative layouts/UI - Projects/Instagram_Login_UI/app/src/main/java/com/androidkickofftraining/instagram_login_ui/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.androidkickofftraining.instagram_login_ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.instagram_login)
+    }
+}
